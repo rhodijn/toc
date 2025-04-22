219eb808ae5f57c0fa2bf51434d4ce937fa9cd28
Rhodijn <roger.zollinger@proton.me>
2025-04-22 17:45:59 +0200

    update main.py

--- test.py
+++ test.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
-def say_hi():
-    print('hi')
+def say_hi(t):
+    print(t + '\n')
 
 if __name__ == '__main__':
-    say_hi()
+    say_hi('hi')
+
+    i = 0
+
+    while None is not True:
+        i += 1
+        if i > 10:
+            print('\n')
+            break
+        print(i, end=' ')
+
+    say_hi('bye')

877aa4931390d1781e11445f414095a4cdd9d465
Rhodijn <roger.zollinger@proton.me>
2025-04-23 08:11:30 +0200

    add move.py

---
+++ move.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import os, project_data, re
+
+file_list = sorted(os.listdir(project_data.TOC_PATH))
+
+for f in file_list:
+    if not re.search('^\d+.pdf', f):
+        file_list.remove(f)
+
+print(file_list)

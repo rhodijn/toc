0e308eabd60762d42dc21380f26371c4a51e70e2
Zollinger Roger (zolo) <zolo@zhaw.ch>
2025-04-25 11:54:37 +0200

    use dict instead of 2 lists for files

--- ftp.py
+++ ftp.py
@@ -2,23 +2,23 @@
 
 import os, paramiko, project_data, re
 
-f_local = []
-f_processed = []
+f_processed = {}
 f_remote = []
 
-def find_toc(f_local, path):
+def find_toc(f_processed, path):
+    f_local = []
     f_local = sorted(os.listdir(path))
 
     for f in f_local:
-        if not re.search('^\\d+.pdf', f):
-            f_local.remove(f)
+        if re.search ('^\\d+.pdf', f):
+            f_processed[f] = False
     
     print('local files to process: ', end='')
-    print(f_local)
+    print(f_processed.keys())
 
-    return f_local
+    return f_processed
 
-def upload_toc(f_local, f_remote):
+def upload_toc(f_processed, f_remote):
     host_name = project_data.FTP_HOST
     port = project_data.FTP_PORT
     user_name = project_data.USER
@@ -31,37 +31,36 @@ def upload_toc(f_local, f_remote):
     sftp_client = ssh_client.open_sftp()
     print("connection established ... ")
 
-    f_remote = sftp_client.listdir(project_data.P_REMOTE)
+    f_remote = sftp_client.listdir(project_data.P_REMOTE + project_data.P_WIN)
 
-    for f in f_local:
+    for f in f_processed.keys():
         if f in f_remote:
             print('file ' + f  + ' is already online (will not be replaced)')
             continue
         try:
-            sftp_client.put(project_data.P_LOCAL + f, project_data.P_REMOTE + f)
-            f_processed.append(f)
+            sftp_client.put(project_data.P_LOCAL + f, project_data.P_REMOTE + project_data.P_WIN + f)
+            f_processed[f] = True
         except OSError as e:
             print(e)
 
         print('files uploaded: ', end='')
-        print(f_processed)
+        print(f_processed.keys())
 
-    print(f'remote files: {sftp_client.listdir(project_data.P_REMOTE)}')
+    print(f'remote files: {sftp_client.listdir(project_data.P_REMOTE + project_data.P_WIN)}')
 
     sftp_client.close()
     ssh_client.close()
 
-    return f_local, f_remote
+    return f_processed, f_remote
 
-def move_toc(f_local, f_processed):
-    for f in f_processed:
-        os.rename(project_data.P_LOCAL + f, project_data.P_ARCHIVE + f)
-    
-    f_local = find_toc(f_local, project_data.P_LOCAL)
+def move_toc(f_processed):
+    for f in f_processed.keys():
+        if f_processed[f]:
+            os.rename(project_data.P_LOCAL + f, project_data.P_ARCHIVE + f)
 
-    return f_local, f_processed
+    return f_processed
 
 if __name__ == '__main__':
-    f_local = find_toc(f_local, project_data.P_LOCAL)
-    f_local, f_remote = upload_toc(f_local, f_remote)
-    f_local, f_processed = move_toc(f_local, f_processed)
+    f_processed = find_toc(f_processed, project_data.P_LOCAL)
+    f_processed, f_remote = upload_toc(f_processed, f_remote)
+    f_processed = move_toc(f_processed)

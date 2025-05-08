8aed5b45451d1871da8549e4ad518b6d74699d43
Rhodijn <roger.zollinger@proton.me>
2025-05-08 15:59:08 +0200

    update filename of .json (include year)

--- upload.py
+++ upload.py
@@ -180,4 +180,4 @@ if __name__ == '__main__':
     if f_process[f_name]['valid']:
         f_process = upload_toc(f_process, f_name, p_local, project_data.P_WIN)
     f_process = rm_toc(f_process, f_name, p_local)
-    f_process = write_json(f_process, project_data.P_LOG, 'toc_log.json')
+    f_process = write_json(f_process, project_data.P_LOG, f'toc_log_{datetime.datetime.now().strftime("%Y")}.json')

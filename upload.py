e35acfcd96084aa4f704989539ad63f645372bf0
Rhodijn <roger.zollinger@proton.me>
2025-05-12 14:31:13 +0200

    optimize spacing / layout

--- upload.py
+++ upload.py
@@ -217,6 +217,9 @@ def write_json(f_process: dict, p_log: str, f_log: str) -> dict:
 
 
 if __name__ == '__main__':
+    """
+    This is the __main__ routine, it controls the entire process
+    """
     args = get_file()
     f_process, f_toc, p_lib = check_toc(
         args.file,

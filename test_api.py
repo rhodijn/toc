71f56c98b7aed326400baf07b2cdcf5ca7b2663f
Rhodijn <roger.zollinger@proton.me>
2025-05-17 12:40:44 +0200

    multi-line f-string

--- test_api.py
+++ test_api.py
@@ -5,7 +5,8 @@ import xml.etree.ElementTree as ET
 
 mms_id = 991030022319705501
 
-query = requests.get(f'{project_data.API_URL}{mms_id}?view={project_data.API_VIEW}&expand={project_data.API_EXP}&apikey={project_data.API_KEY}&format=json')
+query = requests.get(f'{project_data.API_URL}{mms_id}?view={project_data.API_VIEW}&expand={project_data.API_EXP} \
+                     &apikey={project_data.API_KEY}&format={project_data.API_FRMT}')
 
 data = json.loads(query.content.decode(encoding='utf-8'))
 

e865b24a99ebbdf36b9c517f73ac55bb48ec2358
Rhodijn <roger.zollinger@proton.me>
2025-05-17 12:32:13 +0200

    change response to json, write response to json-file

--- test_api.py
+++ test_api.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python3
 
-import project_data, requests
+import json, project_data, requests
 import xml.etree.ElementTree as ET
 
 mms_id = 991030022319705501
 
-alma_query = requests.get(f'{project_data.API_URL}{mms_id}?view={project_data.API_VIEW}&expand={project_data.API_EXP}&apikey={project_data.API_KEY}')
+query = requests.get(f'{project_data.API_URL}{mms_id}?view={project_data.API_VIEW}&expand={project_data.API_EXP}&apikey={project_data.API_KEY}&format=json')
 
-with open('log/test.xml', 'wb') as f:
-    f.write(alma_query.content)
+data = json.loads(query.content.decode(encoding='utf-8'))
 
-tree = ET.parse('log/test.xml')
-root = tree.getroot()
-
-for item in root.findall('./'):
-    for child in item:
-        for c in child:
-            print(f'{item.tag.split("}")[1]}: {child.tag.split("}")[1]}: {c.tag.split("}")[1]}')
-            if c.tag.split('}')[1] == 'errorMessage':
-                print(c.text.encode('utf-8'))
+with open('log/query.json', mode='w', encoding='utf-8') as f:
+    f.seek(0)
+    json.dump(data, f, indent=4)

3e7184d6987af92253345df21582b02253df4197
Rhodijn <roger.zollinger@proton.me>
2025-05-19 11:26:47 +0200

    trying to receive a message from put

--- test_api.py
+++ test_api.py
@@ -4,8 +4,7 @@ import json, project_data, requests
 
 mms_id = 991030022319705501
 
-get_url = f'{project_data.API_URL}{mms_id}?view={project_data.API_VIEW}&expand={project_data.API_EXP} \
-    &apikey={project_data.API_KEY}&format={project_data.API_FRMT}'
+get_url = f'{project_data.API_URL}{mms_id}{project_data.API_PARA_GET}&apikey={project_data.API_KEY}&format={project_data.API_FRMT}'
 
 query = requests.get(get_url)
 
@@ -15,11 +14,11 @@ with open('log/get.json', mode='w', encoding='utf-8') as f:
     f.seek(0)
     json.dump(data, f, indent=4)
 
-put_url = f'{project_data.API_URL}{mms_id}?validate=true&override_warning=true&override_lock=true&stale_version_check=false&cataloger_level=20&check_match=false&apikey={project_data.API_KEY}'
+put_url = f'{project_data.API_URL}{mms_id}{project_data.API_PARA_PUT}&apikey={project_data.API_KEY}'
 
 response = requests.put(put_url, data=project_data.API_BDY)
 
 with open('log/put.txt', mode='w') as f:
     f.write(str(response))
 
-# print(response.decode('utf-8'))
+print(response)

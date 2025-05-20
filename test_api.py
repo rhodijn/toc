a097afcc7ba9822e2b2f84af8d591fe3b70349fd
Zollinger Roger (zolo) <zolo@zhaw.ch>
2025-05-20 08:58:40 +0200

    api first result with get!

--- test_api.py
+++ test_api.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python3
 
-import json, project_data, requests
+from lxml import etree
+import project_data, os, requests
 
-mms_id = 991112402779705501
+mms_id = 9947393580105520
 url = 'https://slspmedia.hsb.zhaw.ch/public/swisscovery/inhaltsverzeichnis/winterthur/995860000000545473.pdf'
 
 get_url = f'{project_data.API_URL}{mms_id}{project_data.API_PARA_GET}&apikey={project_data.API_KEY}&format={project_data.API_FRMT}'
 
 query = requests.get(get_url)
 
-data = json.loads(query.content.decode(encoding='utf-8'))
+data = query.content.decode(encoding='utf-8')
 
-with open('log/get.json', mode='w', encoding='utf-8') as f:
-    f.seek(0)
-    json.dump(data, f, indent=4)
+with open('log/tmp.xml', mode='w', encoding='utf-8') as f:
+    f.write(data)
 
 put_url = f'{project_data.API_URL}{mms_id}{project_data.API_PARA_PUT}&apikey={project_data.API_KEY}'
 
 response = requests.put(put_url, headers=project_data.API_HDR, data=f'{project_data.API_BDY_1}{url}{project_data.API_BDY_2}')
 
-with open('log/put.txt', mode='w') as f:
+with open('log/put.xml', mode='w') as f:
     f.write(str(response))
 
-print(response)
+tmp = etree.parse('log/tmp.xml')
+new_xml = etree.tostring(tmp, pretty_print = True, encoding = str)
 
-print(f'{project_data.API_BDY_1}{url}{project_data.API_BDY_2}')
+with open('log/get.xml', mode='w') as f:
+    f.write(new_xml)
+
+os.remove('log/tmp.xml')

7ff21f36e1718380c276b86b6bc21e1dbe55e3da
Rhodijn <roger.zollinger@proton.me>
2025-05-27 10:45:52 +0200

    update version numbers

--- modules/apihandler.py
+++ modules/apihandler.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #
 #   ##################      this module communicates with alma
-#   ##                ##    version 0.7 (2025-05-26)
+#   ##                ##    version 0.5 (2025-05-27)
 #   ##              ##
 #     ######      ##
 #       ##      ######

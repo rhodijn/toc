1139dd050ddc34142a21df992efdfa0df3f49c7b
Zollinger Roger (zolo) <zolo@zhaw.ch>
2025-06-06 13:07:42 +0200

    update version info in xmlhandler.py

--- modules/xmlhandler.py
+++ modules/xmlhandler.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #
 #   ###################      this module handles xml data
-#   ##                 ##    version 0.1 (2025-06-02)
+#   ##                 ##    version 0.7 (2025-06-06)
 #   ##               ##
 #     ######       ##
 #       ##       ######
